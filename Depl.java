a29e75b7ca3190fcd8e992251e8815e752983444
collision-hp <champatisatyabrata26@gmail.com>
2025-08-17 12:56:34 +0530

    Bug fixes

--- Depl.java
+++ Depl.java
@@ -1,18 +1,23 @@
-// 6. Write a program to print duplicate characters 
-// from the given String? 
-//  Ex: Programming  Output:  r, g, m 
+// 7. write a program to print all duplicate character 
+// and their count form the given String? 
+//  Ex: Programmming  Output: g: 2 
+//                            r: 2
+//                            m: 3
 
 public class Depl {
     public static String printDupl_String(String str){
         String newstr="";
         for(int i=0;i<str.length();i++){
-            int count=0;
+            int count=1;
             for(int j=i+1;j<str.length();j++){
                 if(str.charAt(i)==str.charAt(j)){
+                    if(str.charAt(i)==str.charAt(i-1)){
+                        break;
+                    }
                     count++;
                 }
             }
-            if(count==1){
+            if(count>1){
                 System.out.println(str.charAt(i));
                 newstr+=str.charAt(i)+":"+count+"\n";
             }
