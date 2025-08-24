1517a471a0d407a39feab36fd82fcab5f4b62a5b
collision-hp <champatisatyabrata26@gmail.com>
2025-08-24 23:54:48 +0530

    2qs

---
+++ clockwise_anticlockwise.java
@@ -0,0 +1,17 @@
+// 33. program of clockwise and anticlockwise 
+       
+// Input                clockwise  Output:                
+// 1    2    3             7    4    1                     
+// 4    5    6             8    5    2                     
+// 7    8    9             9    6    3 
+
+public class clockwise_anticlockwise {
+    public static int[][] rotate(int arr[][], int r, int c){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                
+            }
+        }
+        return;
+    }
+}
--- permutations.java
+++ permutations.java
@@ -2,19 +2,18 @@
 //      input:- "abc" 
 //      output:-abc,acb,bac,bca,cab,cba
 public class permutations {
-    public static void permute(String str, String prefix) {
-        if (str.length() == 0) {
-            System.out.println(prefix); // Base case: no characters left
+    public static void permute(String str, String result) {
+        if (str.isEmpty()) {
+            System.out.println(result); // Base case: no characters left in remaining
         } else {
             for (int i = 0; i < str.length(); i++) {
                 // Choose character at index i
                 char ch = str.charAt(i);
-
                 // Remaining string after removing chosen character
                 String remaining = str.substring(0, i) + str.substring(i + 1);
 
                 // Recurse with new prefix and remaining string
-                permute(remaining, prefix + ch);
+                permute(remaining, result + ch);
             }
         }
     }
@@ -24,5 +23,5 @@ public class permutations {
         System.out.println("All permutations of " + input + " are:");
         permute(input, "");
     }
-
+//done in copy last page
 }
