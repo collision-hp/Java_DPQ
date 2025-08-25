aaad997ec795255957b6b6add72c8564c5014a47
collision-hp <champatisatyabrata26@gmail.com>
2025-08-25 23:45:08 +0530

    2qs

---
+++ anticlockwise.java
@@ -0,0 +1,29 @@
+// program of anticlockwise 
+
+// Input                  anticlockwise Output: 
+// 1    2    3             3    6    9 
+// 4    5    6             2    5    8 
+// 7    8    9             1    4    7
+
+public class anticlockwise {
+    public static int[][] anticlockwiseArray(int arr[][], int r, int c) {
+        int newarr[][] = new int[r][c];
+        int n=c;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                newarr[n-1-j][i]=arr[i][j];
+            }
+        }
+        return newarr;
+    }
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int newarr[][]=anticlockwiseArray(arr, 3, 3);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(newarr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
--- clockwise_anticlockwise.java
+++ clockwise_anticlockwise.java
@@ -7,11 +7,25 @@
 
 public class clockwise_anticlockwise {
     public static int[][] rotate(int arr[][], int r, int c){
+        int arrnew[][]=new int[r][c];
+        int n=c;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                
+                arrnew[j][n-1]=arr[i][j];
             }
+            n--;
         }
-        return;
+        return arrnew;
+
+    }
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int newarr[][]=rotate(arr, 3, 3);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(newarr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
