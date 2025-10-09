82ab2b8329e47cbe983357c739c1ed04f19d6e6f
collision-hp <champatisatyabrata26@gmail.com>
2025-10-09 22:02:44 +0530

    Minor bug fixes

--- Part-2/String/UniqueChars.java
+++ Part-2/String/UniqueChars.java
@@ -1,26 +1,30 @@
 package String;
-import java.util.HashSet;
+import java.util.*;
 import java.util.Set;
 
 public class UniqueChars{
     public static String unique(String str){
-        str=str.replace(" ", "");
-        Set <Character> charset=new HashSet<>();
+        // str=str.replace(" ", "");
+        Set <Character> charset=new LinkedHashSet<>();
         // charset.add(str.charAt(0));
         StringBuilder strnew=new StringBuilder();
         for(char i:str.toCharArray()){
-            if(charset.contains(i)){
-                continue;
-            }
-            else{
-                strnew.append(i);
-                charset.add(i);
-            }
+            charset.add(i);
+            // if(charset.contains(i)){
+            //     continue;
+            // }
+            // else{
+            //     strnew.append(i);
+            //     charset.add(i);
+            // }
+        }
+        for(char i:charset){
+            System.out.print(i);
         }
         return strnew.toString();
     }
     public static void main(String[] args) {
-        
-        System.out.println(unique("Java Automation"));
+        // System.out.println(unique("Java Automation"));
+        unique("Java Automation");
     }
 }
