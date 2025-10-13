8da6ac767a4b6b125b761df848ef63f9b7a9b6a7
collision-hp <champatisatyabrata26@gmail.com>
2025-10-13 13:18:36 +0530

    2updates

--- Capgemini/Find_GCD_using_Euclidean_Algorithm.java
+++ Capgemini/Find_GCD_using_Euclidean_Algorithm.java
@@ -2,7 +2,6 @@
 
 public class Find_GCD_using_Euclidean_Algorithm {
     public static int gcd(int a,int b){    //2,5
-
         while(b!=0){                           //5 , 2
             int temp=b;                        //temp=5 , 2 , 3 , 2 , 1 , 1
             b=a%b;                             //b=2 , 3 , 2 , 1 , 1 , 0
@@ -10,19 +9,7 @@ public class Find_GCD_using_Euclidean_Algorithm {
         }
         return a;
     }
-
-    public static int gcdMethod_2(int a,int b){
-        int max=Math.max(a, b);
-        int lcm=max;
-        while(true){
-            if(lcm%a==0 && lcm%b==0){
-                return lcm;
-            }
-            lcm++;
-        }
-    }
     public static void main(String[] args) {
         System.out.println(gcd(2, 5));
-        System.out.println(gcdMethod_2(2, 6));
     }
 }
--- Capgemini/find_LCM.java
+++ Capgemini/find_LCM.java
@@ -1,5 +1,14 @@
 public class find_LCM {
+
     public static int LCM(int a,int b,int c){
+        int lcm=Math.max(a,(Math.max(b, c)));
+        while(lcm%a!=0 || lcm%b!=0 || lcm%c!=0){
+            lcm++;
+        }
+        return lcm;
+    }
+
+    public static int LCM_M2(int a,int b,int c){
         int max=Math.max(a,Math.max(c, b));
         int lcm=max;
         while(true){
@@ -11,6 +20,6 @@ public class find_LCM {
     }
     public static void main(String[] args) {
         System.out.println(LCM(2, 3, 4));
-        System.out.println(LCM(4, 5, 6));
+        System.out.println(LCM_M2(4, 5, 6));
     }
 }
