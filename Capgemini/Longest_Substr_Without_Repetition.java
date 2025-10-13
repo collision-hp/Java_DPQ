8aa6293453dfa97350c5b0b46ff7f64a60900582
collision-hp <champatisatyabrata26@gmail.com>
2025-10-13 19:11:40 +0530

    2qs

--- Capgemini/Longest_Substr_Without_Repetition.java
+++ Capgemini/Longest_Substr_Without_Repetition.java
@@ -33,7 +33,34 @@ public class Longest_Substr_Without_Repetition {
         }
         return finalstr;
     }
+    public static String longestSubstr_M2(String str){
+        String substr="";
+        Set<Character> set = new HashSet<>();
+        for(char ch:str.toCharArray()){
+            set.add(ch);
+        }
+        int len=set.size();
+        Set<Character> setnew=new LinkedHashSet<>();
+        for(int i=0;i<str.length();i++){
+            setnew.clear();
+            setnew.add(str.charAt(i));
+            for(int j=i+1;j<str.length();j++){
+                if(setnew.contains(str.charAt(j))){
+                    break;
+                }
+                setnew.add(str.charAt(j));
+                if(setnew.size()==len){
+                    for(char ch:setnew){
+                        substr+=ch;
+                    }
+                    return substr;
+                }
+            }
+        }
+        return "Not found";
+    }
     public static void main(String[] args) {
         System.out.println(longestSubstr("abcabdcbb"));
+        System.out.println(longestSubstr_M2("abcabdcbb"));
     }
 }
