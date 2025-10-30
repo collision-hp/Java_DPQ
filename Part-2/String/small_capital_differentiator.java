8b5f8128f77b4ef664354648d71cb8aa180ac0d1
collision-hp <champatisatyabrata26@gmail.com>
2025-10-30 13:00:02 +0530

    1 update

--- Part-2/String/small_capital_differentiator.java
+++ Part-2/String/small_capital_differentiator.java
@@ -13,7 +13,22 @@ public class small_capital_differentiator{
         System.out.println("Output in lowercase: " + lowerCase);
         System.out.println("Output in uppercase " + upperCase);
     }
+    public static void Separator(String str){
+        String strlower="";
+        String strupper="";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)>='A' && str.charAt(i)<='Z'){
+                strupper+=str.charAt(i);
+            }
+            else{
+                strlower+=str.charAt(i);
+            }
+        }
+        System.out.println(strlower);
+        System.out.println(strupper);
+    }
     public static void main(String[] args) {
         differentiator("aBACbcEDed");
+        Separator("aBACbcEDed");
     }
 }
